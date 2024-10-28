3b30f4eb614ba16e82c11ab282a12488d6af351e
Dandu Srija <dandusrija2503@gmail.com>
2024-10-28 18:57:04 +0530

    Performed an operation to calculate the POWER of a particular number.

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,11 @@
 package com.qualizeal;
 
+import com.qualizeal.javaapp09.operation.PowerOfNumber;
+ 
 public class App {
 	public static void main(String[] args) {
-		
-	}
+		PowerOfNumber powerOfNumber = new PowerOfNumber();
+		int res = powerOfNumber.calculatePowerOfNumber(6,2);
+		System.out.println(res);
+	 }
 }
---
+++ src/main/java/com/qualizeal/javaapp09/operation/PowerOfNumber.java
@@ -0,0 +1,24 @@
+package com.qualizeal.javaapp09.operation;
+ 
+public class PowerOfNumber {
+	public String definePowerOfNumber() {
+		return "Power of number";
+	}
+ 
+	public int calculatePowerOfNumber(int base, int n) {
+		if (base < 0 || n < 0) {
+			return -1;
+		} else if (base == 0 || n == 0) {
+			return -2;
+		} else {
+			int result = 1;
+ 
+			while (n != 0) {
+				result = result * base;
+				n--;
+			}
+ 
+			return result;
+		}
+	}
+}
