8a18b3299b0552a4a88aa8e6a503d11b59ec68df
Ramya Mudragalla <mudragallaramya28@gmail.com>
2024-10-28 18:20:11 +0530

    PowerOfNumber

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,16 @@
 package com.qualizeal;
 
+import com.qualizeal.javaapp09.operation.PowerOfNumber;
+
+
 public class App {
 	public static void main(String[] args) {
+		
+		PowerOfNumber powerOfNumber = new PowerOfNumber();
+		int result = powerOfNumber.calculatePowerOfNumber(10 , -2);
+		
+		System.out.println(result);
+		
 		
 	}
 }
