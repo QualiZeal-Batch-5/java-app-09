729a8dab9828bc5c0781ea01debce1add8daf925
Sowmya <sowmyanimmala11@gmail.com>
2024-10-28 19:55:15 +0530

    Javaapp09

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,11 @@
 package com.qualizeal;
 
+import com.qualizeal.javaapp09.operation.PowerOfNumber;
+
 public class App {
 	public static void main(String[] args) {
-		
+		PowerOfNumber powerOfNumber = new PowerOfNumber();
+		int result = powerOfNumber.calculatePowerOfNumber(2,3);
+		System.out.println(result);
 	}
 }
---
+++ src/main/java/com/qualizeal/javaapp09/operation/PowerOfNumber.java
@@ -0,0 +1,25 @@
+package com.qualizeal.javaapp09.operation;
+
+public class PowerOfNumber {
+
+	public String definePowerOfNumber() {
+		return "It indicates how many times the base is multiplied by itself.";
+	}
+
+	public int calculatePowerOfNumber(int num1 , int num2) {
+		if(num1 < 0 || num2 < 0) {
+			return -1;
+		}
+		else if(num1 == 0 || num2 == 0) {
+			return -2;
+		}
+		else {
+			int ans = 1;
+			while(num2 != 0) {
+				ans = ans * num1;
+				num2--;
+			}
+			return ans;
+		}
+	}
+}
