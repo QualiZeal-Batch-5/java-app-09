c04da36a363316359bbfd312d5d023168856f180
Likhitha Boorsu <twinkleboorsu1717@gmail.com>
2024-10-28 18:54:03 +0530

    addingfiles

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+
+package com.qualizeal;
+
+import com.qualizeal.javaapp09.operation.PowerOfNumber;
+
+public class App {
+	public static void main(String[] args) {
+		PowerOfNumber powerOfNumber = new PowerOfNumber();
+		System.out.println(powerOfNumber.calculatePowerOfNumber(5, 2));
+		System.out.println(powerOfNumber.definePowerOfNumber());
+
+	}
+}
--- src/main/java/com/qualizeal/App.java
+++
@@ -1,7 +0,0 @@
-package com.qualizeal;
-
-public class App {
-	public static void main(String[] args) {
-		
-	}
-}
