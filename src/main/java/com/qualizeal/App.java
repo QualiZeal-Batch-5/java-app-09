ca5105fa62ae74048e9b793bdcc19225356f1039
Shravani <sravanig066@gmail.com>
2024-10-28 18:33:33 +0530

    Power of a number

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,10 @@
 package com.qualizeal;
 
+import com.qualizeal.javaapp09.operation.PowerOfNumber;
+
 public class App {
 	public static void main(String[] args) {
-		
+		PowerOfNumber powerOfNumber = new PowerOfNumber();
+		System.out.println(powerOfNumber.calculatePowerOfNumber(5, 3));
 	}
 }
