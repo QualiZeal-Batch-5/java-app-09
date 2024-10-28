d201870f5a69e66443933eb554b5a4e15f825527
sushmitha <heysushmitha@gmail.com>
2024-10-28 18:31:08 +0530

    PowerOfNumber

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,13 @@
 package com.qualizeal;
 
+import com.qualizeal.javaapp09.operation.PowerOfNumber;
+
 public class App {
 	public static void main(String[] args) {
-		
+		PowerOfNumber powerOfNumber = new PowerOfNumber();
+
+		int result=powerOfNumber.calculatePowerOfNumber(2, 4);
+		System.out.println(result);
+
 	}
 }
---
+++ src/main/java/com/qualizeal/javaapp09/operation/PowerOfNumber.java
@@ -0,0 +1,25 @@
+package com.qualizeal.javaapp09.operation;
+
+public class PowerOfNumber {
+	public String definePowerOfNumber() {
+		return "The power of a number is the result of multiplying it by itself a specified number of times, expressed as a^n ";
+	}
+
+	public int calculatePowerOfNumber(int base, int num) {
+		if (base < 0 || num < 0) {
+			return -1;
+		} else if (base == 0 || num == 0) {
+			return -2;
+		} else {
+			int result = 1;
+
+			while (num != 0) {
+				result = result * base;
+				num--;
+			}
+
+			return result;
+		}
+	}
+
+}
