7dda7b3523ab21551e5ccd69e6a292ef912e432e
Disney Pallavi Bezawada <disneypallavi@gmail.com>
2024-10-28 17:59:49 +0530

    Adding Files.

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,16 @@
 package com.qualizeal;
 
+import com.qualizeal.javaapp09.operation.PowerOfNumber;
+
 public class App {
+	
 	public static void main(String[] args) {
 		
+		PowerOfNumber powerOfNumber = new PowerOfNumber();
+		
+		System.out.println(powerOfNumber.calculatePowerOfNumber(3, 5));
+		
+		System.out.println(powerOfNumber.definePowerOfNumber());
+
 	}
 }
---
+++ src/main/java/com/qualizeal/javaapp09/operation/PowerOfNumber.java
@@ -0,0 +1,38 @@
+package com.qualizeal.javaapp09.operation;
+
+public class PowerOfNumber 
+{
+	
+	public String definePowerOfNumber() 
+	{
+		
+		return "It indicates how many times the base is multiplied by itself.";
+		
+	}
+	
+	public int calculatePowerOfNumber(int num1 , int num2) 
+	{
+		
+		if(num1 < 0 || num2 < 0) 
+		{
+			return -1;
+		}
+		else if(num1 == 0 || num2 == 0) 
+		{
+			return -2;
+		}
+		else
+		{
+			int result=1;
+			
+			while(num2 != 0) 
+			{
+				result=result*num1;
+				num2--;
+			}
+			
+			return result;
+		}
+	}
+
+}
