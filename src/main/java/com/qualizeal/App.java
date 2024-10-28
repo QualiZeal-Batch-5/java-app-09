6aa9e561fa6d7100bf095da82ee8e10d6d9ce96d
Bandaru Udaysri <bandaruudaysri2002@gmail.com>
2024-10-28 18:05:02 +0530

    Added Solution

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,12 @@
 package com.qualizeal;
 
+import com.qualizeal.javaapp09.operation.PowerOfNumber;
+
 public class App {
 	public static void main(String[] args) {
-		
+		PowerOfNumber powerOfNumber = new PowerOfNumber();
+		System.out.println(powerOfNumber.calculatePowerOfNumber(5, 2));
+		System.out.println(powerOfNumber.definePowerOfNumber());
+
 	}
 }
