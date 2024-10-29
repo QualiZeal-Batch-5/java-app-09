6c1b6bce1341e01ec44fe0099814a164b4eb7265
Poloju Lahari <poloju.lahari@qualizeal.com>
2024-10-29 14:38:43 +0530

    Power of Number

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,11 @@
 package com.qualizeal;
 
+import com.qualizeal.javaapp09.operation.PowerOfNumber;
+
 public class App {
 	public static void main(String[] args) {
-		
-	}
+		PowerOfNumber powerOfNumber = new PowerOfNumber();
+		int res = powerOfNumber.calculatePowerOfNumber(7, 2);
+		System.out.println(res);
+	 }
 }
